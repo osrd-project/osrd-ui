fd1eac7d4dfa8ed2515bf9d65cb497e034aac1dd
Mathieu Richard <mathieu.richard747@gmail.com>
2024-03-19 16:32:07 +0100

    ui-core: checbox: prevent parentChildrenMap and childrenMap to be recalculated on each update

--- ui-core/src/App.tsx
+++ ui-core/src/App.tsx
@@ -4,10 +4,15 @@ import Tree from './components/inputs/checkbox/Tree';
 function App() {
   return (
     <div >
-      <Tree small items={[
-    {id: 1, name: 'foo', items: [
-        {id: 2, name: 'foo2', items: [{id: 3, name: 'foo3'}, {id: 4, name: 'foo4'}]}, 
-        {id: 5, name: 'foo5', items: [{id: 6, name: 'foo6'}]}
+      <Tree  items={[
+    {id: 1, 
+    name: 'foo', 
+    items: [
+        {
+          id: 2, 
+          name: 'foo2',
+          items: [{id: 3, name: 'foo3'}, {id: 4, name: 'foo4'}]}, 
+        {id: 5, name: 'foo5', items: [{id: 6, name: 'foo6', items: [{id: 8, name: 'foo8'}, {id: 9, name: 'foo9', }]}]}
     ]},
     {id: 7, name: 'foo7'}
 ]} />
