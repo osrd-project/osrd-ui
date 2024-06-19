fd54b206600f1d7b88f44e7f2cf879749243d3e5
nncluzu <ngamenichaka@yahoo.fr>
2024-06-19 20:53:00 +0200

    ui-core, datePicker: test useDatePicker

---
+++ ui-core/src/components/inputs/datePicker/__tests__/useDatePicker.spec.ts
@@ -0,0 +1,54 @@
+import { renderHook, act } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { CalendarSlot } from '../type';
+import useDatePicker from '../useDatePicker';
+
+describe('useDatePicker', () => {
+  describe('day click on calendar handling', () => {
+    describe('single mode', () => {
+      it('should set input value with a formatted selected slot', () => {
+        const { result } = renderHook(() =>
+          useDatePicker({
+            calendarPickerProps: { mode: 'single' },
+            inputProps: { id: 'id', label: 'single' },
+          })
+        );
+        const newSelectedSlot = { start: new Date(2024, 0, 1), end: null };
+
+        act(() => {
+          result.current.handleCalendarPickerChange(newSelectedSlot);
+        });
+
+        expect(result.current.selectedSlot).toEqual(newSelectedSlot);
+        expect(result.current.inputValue).toBe('01/01/24');
+      });
+    });
+
+    describe('range mode', () => {
+      it('should set input value with a formatted selected slot', () => {
+        const { result } = renderHook(() =>
+          useDatePicker({
+            calendarPickerProps: { mode: 'range' },
+            inputProps: { id: 'id', label: 'range' },
+          })
+        );
+        let newSelectedSlot: CalendarSlot = { start: new Date(2024, 0, 1), end: null };
+
+        act(() => {
+          result.current.handleCalendarPickerChange(newSelectedSlot);
+        });
+
+        expect(result.current.selectedSlot).toEqual(newSelectedSlot);
+        expect(result.current.inputValue).toBe('01/01/24 -');
+
+        newSelectedSlot = { start: new Date(2024, 0, 1), end: new Date(2024, 0, 2) };
+        act(() => {
+          result.current.handleCalendarPickerChange(newSelectedSlot);
+        });
+
+        expect(result.current.selectedSlot).toEqual(newSelectedSlot);
+        expect(result.current.inputValue).toBe('01/01 - 02/01/24');
+      });
+    });
+  });
+});
